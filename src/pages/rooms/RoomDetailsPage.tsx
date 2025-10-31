b399f5ee33b3e564eacb74dc6df6dd9931ea2275
Alisson <alisson.oliveiraferreira23@gmail.com>
2025-10-31 19:05:55 -0300

    componentização do RoomDetailsPage

--- src/pages/rooms/RoomDetailsPage.tsx
+++ src/pages/rooms/RoomDetailsPage.tsx
@@ -90,8 +90,6 @@ export function RoomDetailsPage({
     );
   }
 
-  // --- RENDERIZAÇÃO PRINCIPAL ---
-
   return (
     <div className="min-h-screen bg-gray-50">
       <div className="container mx-auto px-4 py-8">
